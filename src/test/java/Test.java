5338660ac9a5d275fc381955bb67f66bf98235c0
bsknblc <basak.balci@ozu.edu.tr>
2022-07-08 14:22:37 +0300

    f

---
+++ src/test/java/Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+
+public class Test {
+    @org.junit.Test
+    public void test() throws InterruptedException {
+        int a = 5;
+        assertEquals(a, 5);
+    }
+}
